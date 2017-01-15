86fddcbbd273f0e81591350accd850313c465a1e
Ryan Van Vechten <vechtenr@gmail.com>
2017-01-15 10:01:28 -0800

    Initial commit of bowling game
    Includes basic bowling game setup:
    -Swipe to launch bowling ball down lane. Ball velocity is determined by
    swipe length.
    -Animation that clears pins from lane.
    -Adjust initial position of ball launch.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+	public Vector3 launchVelocity;
+	private Rigidbody rigidBody;
+	public bool inPlay = false;
+
+	private AudioSource audioSource;
+
+	void Start ()
+	{
+		rigidBody = GetComponent<Rigidbody> ();
+
+		rigidBody.useGravity = false;
+
+		//Launch (launchVelocity);
+
+	}
+
+	public void Launch (Vector3 velocity)
+	{
+		inPlay = true;
+		rigidBody.useGravity = true;
+		rigidBody.velocity = velocity;
+
+		audioSource = GetComponent<AudioSource> ();
+		audioSource.Play ();
+
+	}
+
+
+	void Update ()
+	{
+		
+	}
+}
---
+++ Assets/Scripts/DragLaunch.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(Ball))]
+public class DragLaunch : MonoBehaviour
+{
+
+	private Vector3 dragStart, dragEnd;
+	private float startTime, endTime;
+
+	private Ball ball;
+
+	void Start ()
+	{
+		ball = GetComponent<Ball> ();
+	}
+
+	public void DragStart ()
+	{
+		dragStart = Input.mousePosition;
+		startTime = Time.time;
+	}
+
+	public void DragEnd ()
+	{
+		dragEnd = Input.mousePosition;
+		endTime = Time.time;
+
+		float dragDuration = endTime - startTime;
+
+		float launchSpeedX = (dragEnd.x - dragStart.x) / dragDuration;
+		float launchSpeedZ = (dragEnd.y - dragStart.y) / dragDuration;
+
+		Vector3 launchVelocity = new Vector3 (launchSpeedX, 0f, launchSpeedZ);
+		ball.Launch (launchVelocity);
+	}
+
+	public void MoveStart (float amount)
+	{
+
+		if (!ball.inPlay) {
+			ball.transform.Translate (new Vector3 (amount, 0, 0));
+		}
+
+	}
+
+}
