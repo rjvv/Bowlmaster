ef5e9df29e0bd84d81f96278fac1b28dd3095c5d
Ryan Van Vechten <vechtenr@gmail.com>
2017-01-16 19:29:07 -0800

    adding game logic
    Adding methods to count pins, determine when pins get knocked down,
    animation to reset pins.

---
+++ Assets/Scripts/PinSetter.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PinSetter : MonoBehaviour
+{
+
+	public Text standingDisplay;
+	
+	
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		standingDisplay.text = CountStanding ().ToString ();
+	}
+
+	int CountStanding ()
+	{
+		int standing = 0;
+
+		foreach (Pin pin in GameObject.FindObjectsOfType<Pin>()) {
+			if (pin.IsStanding ()) {
+				standing++;
+			}
+		}
+		return standing;
+	}
+}
